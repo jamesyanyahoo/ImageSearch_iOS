a7654d2c85ce6180984426d1215c537f91d01526
jamesyan <jamesyan@yahoo-inc.com>
2015-09-19 19:33:50 +0800

    Initial Commit

---
+++ RottenTomatoSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoSample
+//
+//  Created by James Yan on 9/19/15.
+//  Copyright © 2015 James Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenTomatoSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoSample
+//
+//  Created by James Yan on 9/19/15.
+//  Copyright © 2015 James Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
